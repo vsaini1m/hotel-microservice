dd32b5beb7fb47721aa3c30fea60f3525a50c6c6
sainivinit <vinitsaini357@gmail.com>
2023-07-01 18:22:17 +0530

    * calling Rating service from User service (UserById->RatingByUserId)

--- UserService/src/main/java/com/vsaini1m/service/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/vsaini1m/service/impl/UserServiceImpl.java
@@ -1,21 +1,27 @@
 package com.vsaini1m.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.vsaini1m.entity.Rating;
 import com.vsaini1m.entity.User;
 import com.vsaini1m.exception.ResourceNotFoundException;
 import com.vsaini1m.repositery.UserRepositery;
 import com.vsaini1m.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService{
 
 	private final UserRepositery userRepositery;
+	private final RestTemplate restTemplate;
 	
 	@Override
 	public User saveUser(User user) {
@@ -34,8 +40,14 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public User getUserById(String userID) {
-		return this.userRepositery.findById(userID)
+		User user = this.userRepositery.findById(userID)
 				.orElseThrow(() -> new ResourceNotFoundException("User with given if not found !! : "+ userID));
+		String url = "http://127.0.0.1:8091/ratings/user/" + user.getId();
+		ArrayList<Rating> ratings = this.restTemplate.getForObject(url, ArrayList.class);
+
+		user.setRatings(ratings);
+
+		return user;
 	}
 
 	@Override
